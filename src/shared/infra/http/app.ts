12d9ee576eb0b782298faa0d9c66942861cacad4
Anderson <andersonfrfilho@gmail.com>
2021-04-24 19:15:01 -0300

    feat: finally project

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -3,20 +3,36 @@ import "dotenv/config";
 import express, { NextFunction, Request, Response } from "express";
 import "express-async-errors";
 import swaggerUi from "swagger-ui-express";
+import * as Sentry from "@sentry/node";
+import * as Tracing from "@sentry/tracing";
 import cors from "cors";
 import { AppError } from "@shared/errors/AppError";
 import createConnection from "@shared/infra/typeorm";
 
 import swaggerFile from "../../../swagger.json";
 import { router } from "./routes";
-
+import rateLimiter from "@shared/infra/http/middlewares/rateLimiter"
 import "@shared/container";
 import upload from "@config/upload";
 
 createConnection();
 const app = express();
 
+app.use(rateLimiter);
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [
+    new Sentry.Integrations.Http({ tracing: true }),
+    new Tracing.Integrations.Express({ app }),
+  ],
+  tracesSampleRate: 1.0,
+});
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
 app.use(express.json());
+
+
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
@@ -25,7 +41,7 @@ app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
 
 app.use(cors());
 app.use(router);
-
+app.use(Sentry.Handlers.errorHandler());
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
   if (err instanceof AppError) {
     return response.status(err.statusCode).json({
---
+++ src/shared/infra/http/middlewares/rateLimiter.ts
@@ -0,0 +1,27 @@
+import redis from 'redis'
+import {RateLimiterRedis} from 'rate-limiter-flexible'
+import { NextFunction, Request,Response } from 'express'
+import { AppError } from '@shared/errors/AppError'
+
+const redisClient = redis.createClient({
+  host:process.env.REDIS_HOST,
+  port:Number(process.env.REDIS_PORT),
+  enable_offline_queue: false
+})
+
+const limiter = new RateLimiterRedis({
+  storeClient: redisClient,
+  keyPrefix: 'rateLimiter',
+  points:10,
+  duration:1
+})
+
+export default async function rateLimiter(request:Request, response: Response,next:NextFunction):Promise<void>{
+  try{
+    await limiter.consume(request.ip)
+    console.log(request.ip)
+    return next();
+  }catch(err){
+    throw new AppError("Too many requests",429)
+  }
+}
