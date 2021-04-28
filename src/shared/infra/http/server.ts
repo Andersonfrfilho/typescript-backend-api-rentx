c56ca0cc1819b446da77a4d0afe992f5df6ff7ec
Anderson <andersonfrfilho@gmail.com>
2021-04-28 09:42:48 -0300

    feat:add socket

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,3 +1,26 @@
-import { app } from "@shared/infra/http/app";
+import express from "express";
+import * as socketio from "socket.io";
+import * as path from "path";
+import cors from "cors";
+const app = express();
+app.use(cors());
+app.set("port", process.env.PORT || 3000);
 
-app.listen(3333, () => console.log("Server is running!"));
+let http = require("http").Server(app);
+// set up socket.io and bind it to our
+// http server.
+let io = require("socket.io")(http);
+
+app.get("/", (req: any, res: any) => {
+  res.sendFile(path.resolve("./client/index.html"));
+});
+
+// whenever a user connects on port 3000 via
+// a websocket, log that a user has connected
+io.on("connection", function(socket: any) {
+  console.log("a user connected");
+});
+
+const server = http.listen(3000, function() {
+  console.log("listening on *:3000");
+});
