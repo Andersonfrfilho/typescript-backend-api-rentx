df0429880df5a3095e2b8fb99c4e31cc3c02dc3f
Anderson <andersonfrfilho@gmail.com>
2021-04-29 17:21:59 -0300

    feat: io-socket

--- src/shared/infra/http/routes/users.routes.ts
+++ src/shared/infra/http/routes/users.routes.ts
@@ -1,11 +1,11 @@
-import { Router } from "express";
+import { request, response, Router } from "express";
 import multer from "multer";
 
 import uploadConfig from "@config/upload";
 import { CreateUserController } from "@modules/accounts/useCases/createUser/CreateUserController";
 import { UpdateUserAvatarController } from "@modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController";
 import { ProfileUserController } from "@modules/accounts/useCases/profileUser/ProfileUserController";
-
+import * as io from "socket.io"
 import { ensureAuthenticated } from "@shared/infra/http/middlewares/ensureAuthenticated";
 import { celebrate, Segments, Joi } from 'celebrate';
 const usersRoutes = Router();
@@ -29,5 +29,11 @@ usersRoutes.patch(
   updateUserAvatarController.handle
 );
 usersRoutes.get("/profile", ensureAuthenticated, profileUserController.handle
+)
+usersRoutes.get("/",async (request: Request, response: Response):Promise<Response> =>{
+
+  request.io.to(request.connected_users['07d115e8-6954-4824-bd91-717cbc3fa5c3']).emit("notification","esse veio")
+  return response.json({api:true});
+}
 )
 export { usersRoutes };
--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -2,14 +2,23 @@
 import { app } from "@shared/infra/http/app"
 import { Server as ServerSocket } from "socket.io"
 import { Server as ServerHttp } from "http"
+import { request } from "express";
 
 app.set("port", process.env.PORT || 3333);
 
 let http = new ServerHttp(app);
 
 const io = new ServerSocket(http);
-
-io.on("connection", function(socket: any) {
+const connected_users:Record<string,string> = {};
+io.on("connection", (socket: any) => {
+  const {user_id} = socket.handshake.query;
+  connected_users[user_id] = socket.id
+  socket.on("disconnect", () =>{
+    delete connected_users[user_id]
+  })
+  io.to(connected_users[user_id]).emit("notification","uhulll")
+  request.io = io
+  request.connected_users = connected_users
   console.log("a user connected");
 });
 
